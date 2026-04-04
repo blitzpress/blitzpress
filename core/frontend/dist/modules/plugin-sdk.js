c730048ecd07de14b3d6843445c8e8bfc87526a1
Andrei Telteu <andrei@telteu.ro>
2026-04-04 19:43:42 +0300

    remove frontend/dist and gitignore it

--- core/frontend/dist/modules/plugin-sdk.js
+++
@@ -1,2 +0,0 @@
-export { c as createEventBus, a as createHookEngine, b as createRegistrar, e as events, g as getRuntimeSnapshot, h as hooks, r as registerPlugin, d as resetRuntimeForTests, f as runtimeState } from './chunks/plugin-runtime.js';
-import './chunks/solid-shared.js';
--- core/frontend/dist/modules/solid-js-store.js
+++
@@ -1,1 +0,0 @@
-export { aU as $RAW, aV as DEV, aW as createMutable, c as createStore, aX as modifyMutable, aY as produce, aZ as reconcile, a_ as unwrap } from './chunks/solid-shared.js';
